13a3bdea161510ae6f20847dd5eafb072637a133
SaiKumar <saikumar.maroj.999@gmail.com>
2019-08-05 18:55:43 +0530

    completed que2

--- src/main/java/main/Question2.java
+++ src/main/java/main/Question2.java
@@ -21,7 +21,6 @@ public static void main(String[] args) {
 }
 public List<Integer> listWithDuplicates(List<Integer> number){
 	List <Integer> L1 = number;
-	//write your code here !! change the L1 to required value
 	return L1 ;
 }
 }
